70487b000bce19d2be4dcf0f7c87f04333e847fc
juancafro <juancsj@hotmail.com>
2018-04-19 00:40:35 -0500

    decrypt-encrypt bouncycastle

--- bouncyCastleTest/Program.cs
+++ bouncyCastleTest/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using FileEncryptionTasks.Helpers.PGP;
+using System;
 
 namespace bouncyCastleTest
 {
     class Program
     {
+        private static string recipientKeyFileName;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string path = "C:\\Users\\juancamilo\\source\\repos\\bouncyCastleTest\\bouncyCastleTest\\files\\";
+            //string inputFileName = path+"ATH_20180308_BOCC_1.txt.pgp";
+            string privateKeyFileName = path+ "elibom-pgp.asc.pri";
+            string passPhrase = "Masivian2018Decrypt";
+            string inputFileName = "CryptedFile";
+            string outputFileName = "decryptedAgain";
+            bool shouldArmor = true;
+            string recipientKeyFileName = path+"elibom-pgp.asc.pri";
+            bool shouldCheckIntegrity = true;
+
+            //PGPEncryptDecrypt.Decrypt(inputFileName,
+            //              privateKeyFileName,
+            //              passPhrase,
+            //              outputFileName);
+
+            PGPEncryptDecrypt.EncryptFile(inputFileName,
+                              outputFileName,
+                              recipientKeyFileName,
+                              shouldArmor,
+                              shouldCheckIntegrity);
+
         }
     }
 }
